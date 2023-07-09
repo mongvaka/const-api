d81ef2e8fe0e6a5f4614d95ec90023157dd47633
sanit-wakchai <sanit.wa@logicspark.com>
2023-07-09 11:35:35 +0700

    10

--- src/app.service.ts
+++ src/app.service.ts
@@ -13,6 +13,6 @@ export class AppService {
   //    return 'Ok'
   // }
   getHello(): string {
-    return 'Hello World test ssh deploy hello auto9';
+    return 'Hello World test ssh deploy hello auto10';
   }
 }
